b33b41b0a68732e33796af927c71c51ba4ff0f6d
awlaska <sofiassantossousa@gmail.com>
2025-11-10 12:10:38 +0000

    Update variaveis.rb

--- variaveis.rb
+++ variaveis.rb
@@ -1,17 +1,18 @@
-# mensagem = "Verificacao concluida"
-# contador = 5
-# puts "#{mensagem} com #{contador} alertas"
+# # 2.1. O que são variáveis em Ruby
+mensagem = "Verificacao concluida"
+contador = 5
+puts "#{mensagem} com #{contador} alertas"
 
-# Contador encontrados
+# # 2.3.  Variáveis Locais
+def verificalog
+    status = "seguro"
+    puts "Status: #{status}"
+end
 
-# def verificalog
-#     status = "seguro"
-#     puts "Status: #{status}"
-# end
+verificalog
 
-# verificalog
 
-# Variáveis de Instância
+# # 2.4. Variáveis de Instância
 # Armazenam o estado de um objeto e começam com @
 class Log                 # Declara uma nova classe (um modelo ou um blueprint) chamada Log
   def initialize(nome)    # Construtor da classe
@@ -25,3 +26,40 @@ end
 
 log1=Log.new("firewall.log")    # Cria uma nova instância da classe Log com o nome do ficheiro "firewall.log"
 log1.mostrar                    # Imprime a mensagem com o nome do ficheiro de log
+
+# # 2.5. Variáveis da Classe
+class Log1
+    @@total_logs = 0
+
+    def initialize(nome)
+            @@total_logs += 1
+    end
+
+    def self.total
+            puts "Total de logs analisados: #{@@total_logs}"
+    end
+end
+
+Log1.new("auth.log")
+Log1.new("newtwork.log")
+Log1.total
+
+# # 2.6. Variáveis Globais
+$versao_script = "1.0"
+
+def mostrar_versao
+    puts "Versão atual: #{$versao_script}"
+end
+
+mostrar_versao
+
+# # 2.7. Constantes
+PI = 3.1419
+NOME_CURSO = "Ciberseguranca Ruby"
+
+puts NOME_CURSO
+
+# # 2.8. Símbolos
+log = {nivel: :erro, mensagem: "Falha de autenticação"}
+puts log[:nivel]
+
