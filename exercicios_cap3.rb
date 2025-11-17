1f2a322b26f0296ab2356689567b560d010d5602
awlaska <sofiassantossousa@gmail.com>
2025-11-17 14:42:23 +0000

    cap 3 exercicios

---
+++ exercicios_cap3.rb
@@ -0,0 +1,22 @@
+# 1
+numero = 25
+resultado = numero.abs
+puts resultado
+
+# 2
+numero = 12
+
+if numero.even?
+  puts "É par"
+else
+  puts "Não é par"
+end
+
+# 3
+numero = 12
+
+if numero.odd?
+  puts "É impar"
+else
+  puts "Não é ímpar"
+end
